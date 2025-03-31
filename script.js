c1442fe4083d8a3d56ba3907bf41faa22f8156d7
Sourish Dey <sourish19z@gmail.com>
2025-04-01 00:56:14 +0530

    Add pagination feature and limit api call

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const bttn = document.querySelector(".btn");
 const videoContainer = document.querySelector("#videoContainer");
 const search = document.querySelector(".search-bar");
-// const next = document.querySelector(".nextBttn");
-// const pgNumber = document.querySelector(".pgNumber");
+const next = document.querySelector(".nextBttn");
+const prev = document.querySelector(".prevBttn");
+const pgNumber = document.querySelector(".pgNumber");
+const noElement = document.querySelector("#no-element");
+const noElementContainer = document.querySelector("#no-element-container");
+
+let totalItems = null;
 
 const createElements = (element) => {
   let { viewCount, likeCount, commentCount } = element.items.statistics;
 
   viewCount = new Intl.NumberFormat("en", {
+    // convert 1000 - 1K
     notation: "compact",
     compactDisplay: "short",
   }).format(viewCount);
@@ -36,12 +42,15 @@ const createElements = (element) => {
   videoContainer.appendChild(div);
 };
 
-const apiCall = async () => {
+const apiCall = async (pgNumber) => {
   try {
     const data = await fetch(
-      `https://api.freeapi.app/api/v1/public/youtube/videos?page=1&limit=200`
+      `https://api.freeapi.app/api/v1/public/youtube/videos?page=${pgNumber}&limit=20`
     );
     const fetchedData = await data.json();
+    // totalItems = fetchedData.data.totalItems;
+    // console.log(totalItems);
+
     return fetchedData;
   } catch (error) {
     throw new Error("Cannot fetched data");
@@ -56,23 +65,67 @@ const searchElement = (key, titles) => {
     element.firstElementChild
       .closest(".video")
       .classList.toggle("hide", !isVisible);
+    console.log(isVisible);
+    let hide = document.querySelectorAll(".hide");
+    hide = Array.from(hide).length;
+    if (hide == 20) {
+      noElement.classList.toggle("active");
+    } else {
+      noElement.classList.toggle("hide");
+    }
+    console.log(hide);
   });
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const myData = await apiCall();
+  let myData = await apiCall(pgNumber.innerText);
+  totalItems = myData.data.totalItems;
 
-  const titles = myData.data.data.map((element) => {
+  myData.data.data.map((element) => {
+    // create yt cards
     createElements(element);
   });
+});
 
-  videoTitle = document.querySelectorAll(".title");
-
+search.addEventListener("input", (key) => {
+  const videoTitle = document.querySelectorAll(".title"); // get the title
   const arrayTitles = Array.from(videoTitle).map((element) => {
+    // loop over to get parent element
     return element.parentElement;
   });
+  searchElement(key.target.value, arrayTitles);
+});
 
-  search.addEventListener("input", (key) => {
-    searchElement(key.target.value, arrayTitles);
+next.addEventListener("click", async () => {
+  const flag = Number(pgNumber.textContent);
+  if (flag == Math.ceil(totalItems / 20)) {
+    pgNumber.textContent = 1;
+  } else {
+    pgNumber.textContent = Number(pgNumber.textContent) + 1;
+  }
+  // change video based on pagination
+  const myData = await apiCall(pgNumber.innerText);
+  videoContainer.innerHTML = "";
+
+  myData.data.data.map((element) => {
+    // create yt cards
+    createElements(element);
+  });
+});
+
+prev.addEventListener("click", async () => {
+  const flag = Number(pgNumber.textContent);
+  if (flag == 1) {
+    pgNumber.textContent = Math.ceil(totalItems / 20);
+  } else {
+    pgNumber.textContent = Number(pgNumber.textContent) - 1;
+  }
+  // change video based on pagination
+  const myData = await apiCall(pgNumber.innerText);
+  videoContainer.innerHTML = "";
+
+  myData.data.data.map((element) => {
+    // create yt cards
+    createElements(element);
   });
 });
